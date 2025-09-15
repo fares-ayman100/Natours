1f227369a07cde66d2603453a219c760a0ca87af
Fares Ayman <fareshe73@gmail.com>
2025-09-15 19:56:48 +0300

    update data

--- Models/toursModel.js
+++ Models/toursModel.js
@@ -2,35 +2,39 @@ const mongoose = require('mongoose');
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
-    require: [true, 'A tour must have a name'],
-    required: true,
+    required: [true, 'A tour must have a name'],
     trim: true,
   },
   duration: {
     type: Number,
-    required: [true, 'A tour must have a duratino'],
+    required: [true, 'A tour must have a duration'],
   },
   maxGroupSize: {
     type: Number,
-    required: [true, 'A tour must have a groub size'],
+    required: [true, 'A tour must have a group size'],
   },
   difficulty: {
     type: String,
     required: [true, 'A tour must have a difficulty'],
   },
-  ratingAverage: {
+  ratingsAverage: {
     type: Number,
     default: 4.5,
   },
-  ratingQuantity: {
+  ratingsQuantity: {
     type: Number,
     default: 0,
   },
   price: {
     type: Number,
-    require: [true, 'A tour must have a price'],
+    required: [true, 'A tour must have a price'],
   },
   priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a description'],
+  },
   summary: {
     type: String,
     trim: true,
@@ -45,9 +49,10 @@ const tourSchema = new mongoose.Schema({
     required: [true, 'A tour must have a cover image'],
   },
   images: [String],
-  cratedAt: {
+  createdAt: {
     type: Date,
     default: Date.now(),
+    select: false,
   },
   startDates: [Date],
 });
