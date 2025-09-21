b399860acec8348c33248a8566490f853c2367f7
Fares Ayman <fareshe73@gmail.com>
2025-09-21 19:24:02 +0300

    add signup route

---
+++ routes/usersRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const controller = require('../controllers/usersController');
+const authcontroller = require('../controllers/authController');
+
+const router = express.Router();
+router.route('/signup').post(authcontroller.signup);
+router
+  .route('/')
+  .get(controller.getAllUsers)
+  .post(controller.addUser);
+router
+  .route('/:id')
+  .get(controller.getUser)
+  .patch(controller.updateUser)
+  .delete(controller.deleteUser);
+module.exports = router;
