6a07d9053218a570c159c65b3ff8148d5f4c152d
Fares Ayman <fareshe73@gmail.com>
2025-10-01 01:17:57 +0300

    Sending JWT cookies

--- utils/createSendToken.js
+++ utils/createSendToken.js
@@ -2,7 +2,19 @@ const httpStatus = require('../utils/httpStatus');
 const generateAccessToken = require('../utils/generateAccessToken');
 module.exports = (user, statusCode, res, extra = {}) => {
   const token = generateAccessToken({ id: user._id });
-
+  const cookiOptions = {
+    expires: new Date(
+      Date.now() +
+        process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
+    ),
+    httpOnly: true,
+  };
+  
+  res.cookie('jwt', token, cookiOptions);
+  if (process.env.NODE_ENV == 'production') {
+    cookiOptions.secure = true;
+  }
+  user.password = undefined;
   res.status(statusCode).json({
     status: httpStatus.SUCCESS,
     token,
