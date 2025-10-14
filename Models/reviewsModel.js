54e76c4085a93d1332707f20a825765e5228dda8
Fares Ayman <fareshe73@gmail.com>
2025-10-14 21:44:07 +0300

    Calculating Average rating in tour

--- Models/reviewsModel.js
+++ Models/reviewsModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./toursModel');
 const reviewSchema = new mongoose.Schema(
   {
     review: {
@@ -33,6 +34,39 @@ const reviewSchema = new mongoose.Schema(
   },
 );
 
+reviewSchema.statics.calcAverageRating = async function (
+  tourID,
+) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourID },
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+  //console.log(stats);
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourID, {
+      ratingsAverage: stats[0].avgRating,
+      ratingsQuantity: stats[0].nRating,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourID, {
+      ratingsAverage: 4.5,
+      ratingsQuantity: 0,
+    });
+  }
+};
+
+reviewSchema.post('save', function () {
+  this.constructor.calcAverageRating(this.tour);
+});
+
 // Query Middleware
 reviewSchema.pre(/^find/, function (next) {
   this.populate({
@@ -40,6 +74,13 @@ reviewSchema.pre(/^find/, function (next) {
     select: 'name photo',
   });
   next();
+});
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  this.r = await this.clone().findOne();
+  next();
+});
+reviewSchema.post(/^findOneAnd/, function () {
+  this.r.constructor.calcAverageRating(this.r.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
