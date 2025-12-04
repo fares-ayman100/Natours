86a1aa76da74345a92c60fe58d2c749fb5004364
Fares Ayman <fareshe73@gmail.com>
2025-12-04 20:46:41 +0200

    ImproveStripeImplementation

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -15,7 +15,9 @@ exports.checkoutSesstion = catchAsync(async (req, res, next) => {
   const session = await stripe.checkout.sessions.create({
     mode: 'payment',
     payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}/?tour=${req.params.tourId}&user=${req.user.id}&price=${tour.price}`,
+    success_url: `${req.protocol}://${req.get('host')}`,
+    //success_url: `${req.protocol}://${req.get('host')}/?tour=${req.params.tourId}&user=${req.user.id}&price=${tour.price}`,
+
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
